35deddf27bf127d1be5f92418a5d78e568e2a96c
Donat Treszler <donat12@icloud.com>
2023-11-21 17:01:44 +0100

    Initial Commit

---
+++ Smart Home/Smart_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_HomeApp.swift
+//  Smart Home
+//
+//  Created by Coder on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Smart_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
